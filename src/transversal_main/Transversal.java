e7a059a95562dc83717abac825978867c2de41f0
FedericooCuello <federico_04@outlook.com>
2023-10-15 17:50:24 -0300

    pruebaDeMetodos

--- src/transversal_main/Transversal.java
+++ src/transversal_main/Transversal.java
@@ -15,13 +15,19 @@ public class Transversal {
     
     public static void main(String[] args) {
         Connection conexion = Conexion.getConexion();
-        Alumno a2 = new Alumno(14, 357452825, "Gutierrez", "Maria Agustina", LocalDate.of(1998, 7, 18), true);
+        //Alumno a2 = new Alumno(14, 357452825, "Gutierrez", "Maria Agustina", LocalDate.of(1998, 7, 18), true);
 
         AlumnoData alumData = new AlumnoData();
         //  alumData.guardarAlumno(a2);
         //alumData.modificarAlumno(a2);
-        alumData.eliminarAlumno(14);
-
+        //alumData.eliminarAlumno(14);
+        
+        Alumno alumnoEncontrado = alumData.buscarAlumno(6);
+       
+        System.out.println("Dni :"+alumnoEncontrado.getDni()+ "apellido :"+alumnoEncontrado.getApellido());
+        Alumno alumnoEncontradoPorDni = alumData.buscarAlumnoPorDni(3581575);
+       
+        System.out.println("Dni :"+alumnoEncontradoPorDni.getDni()+ "apellido :"+alumnoEncontradoPorDni.getApellido());
        }
  
        
